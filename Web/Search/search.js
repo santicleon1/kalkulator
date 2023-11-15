8e39f1642937c24357b32168c065aa63e01012f8
santicleon1 <121570106+santicleon1@users.noreply.github.com>
2023-11-15 23:19:19 +0100

    search added

---
+++ Web/Search/search.js
@@ -0,0 +1,4 @@
+function srch() {
+    document.getElementById("m_srch").innerHTML = '<ul id="ul_srch"> <li id="li_srch"> <a href="_blank">Koordinatne sustave.</a> </li> </ul>';
+    document.getElementById("li_srch a").style.display="block";
+}
