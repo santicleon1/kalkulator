2c20a34b8f9ebf213e255aaa388871abcb05fe13
santicleon1 <121570106+santicleon1@users.noreply.github.com>
2023-11-16 18:18:20 +0000

    search updated

--- Web/Search/search.js
+++ Web/Search/search.js
@@ -1,4 +1,9 @@
 function srch() {
-    document.getElementById("m_srch").innerHTML = '<ul id="ul_srch"> <li id="li_srch"> <a href="_blank">Koordinatne sustave.</a> </li> </ul>';
+    document.getElementById("m_srch").innerHTML = '<ul id="ul_srch"> <li id="li_srch"> <a onclick="coord()">Koordinatne sustave.</a> </li> </ul>';
     document.getElementById("li_srch a").style.display="block";
+}
+
+function coord() {
+    document.getElementById("ans").innerHTML = 'Evo link: <a href="_blank" target="_self" style="cursor: pointer; color: white;">Klikni me</a>'
+    document.getElementById("ans").style.display = 'block'
 }
